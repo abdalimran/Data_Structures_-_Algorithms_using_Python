9d632d91e84dd00c3c02ea18765bd3f0cdb1264d
Abdullah Al Imran <abdalimran@live.com>
2017-02-24 19:19:08 +0600

    24-02-2017

---
+++ Linear Data Structures/04 Stack/stack.py
@@ -0,0 +1,67 @@
+class stack:
+	def __init__(self):
+		'''
+		Initializes the stack.
+		'''
+		self.items = []
+	
+	def isEmpty(self):
+		'''
+		Checks if the stack is empty or not.
+		Returns True or False.
+		'''
+		return self.items == []
+	
+	def push(self, item):
+		'''
+		Adds an item into the back of the stack.
+		'''
+		self.items.append(item)
+	
+	def pop(self):
+		'''
+		Removes an item from the back of the stack.
+		'''
+		return self.items.pop()
+	
+	def top(self):
+		'''
+		Returns the top most item of the stack.
+		'''
+		return self.items[len(self.items)-1]
+	
+	def size(self):
+		'''
+		Returns size of the stack.
+		'''
+		return len(self.items)
+	
+	def printStack(self):
+		'''
+		Prints the full stack.
+		'''
+		for item in self.items:
+			print(item, end=' ')
+		
+		
+if __name__=='__main__':
+	
+	'''Example'''
+	
+	stk = stack()
+	stk.push(0)
+	stk.push(1)
+	stk.push(2)
+	stk.push(3)
+	stk.push(4)
+	
+	print("Stack size: ", stk.size())
+	print("Top most item: ", stk.top())
+	
+	stk.pop()
+	stk.push(5)
+	
+	print("Stack size: ", stk.size())
+	print("Top most item: ", stk.top())
+	
+	stk.printStack()
---
+++ Linear Data Structures/05 Queue/queue.py
@@ -0,0 +1,66 @@
+class queue:
+	def __init__(self):
+		'''
+		Initializes the queue.
+		'''
+		self.items = []
+
+	def isEmpty(self):
+		'''
+		Checks if the queue is empty or not.
+		Returns True or False.
+		'''
+		return self.items == []
+
+	def enqueue(self,item):
+		'''
+		Adds an item into the queue.
+		'''
+		self.items.insert(0,item)
+		
+	def dequeue(self):
+		'''
+		Removes an item from the queue.
+		'''
+		self.items.pop()
+		
+	def size(self):
+		'''
+		Returns size of the queue.
+		'''
+		return len(self.items)
+		
+	def top(self):
+		'''
+		Returns the top most item of the queue.
+		'''
+		return self.items[len(self.items)-1]
+	
+	def printQueue(self):
+		'''
+		Prints the full queue.
+		'''
+		for item in self.items:
+			print(item,end=" ")
+			
+if __name__=='__main__':
+	
+	'''Example'''
+	
+	que = queue()
+	que.enqueue(0)
+	que.enqueue(1)
+	que.enqueue(2)
+	que.enqueue(3)
+	que.enqueue(4)
+	
+	print("Queue size: ", que.size())
+	print("Top most item: ", que.top())
+	
+	que.dequeue()
+	que.enqueue(5)
+	
+	print("Queue size: ", que.size())
+	print("Top most item: ", que.top())
+	
+	que.printQueue()
